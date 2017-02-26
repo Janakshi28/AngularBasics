60731484077a3da1661f6fc223467a6c96068957
Janakshi Dulanga <janakshid28@gmail.com>
2017-02-26 05:38:58 +0530

    Add files via upload
    All the source files are uploaded.

---
+++ AddressBookApp/app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module('addressBook', []);
+
+app.controller('saveAddress', function ($scope, addressBookService) {
+    $scope.pageTitle = 'Create New';
+    $scope.addressBook = [];
+    $scope.save = function (firstName,lastName,email) {
+        addressBookService.createAddress(firstName,lastName,email);
+    }
+});
+
+
+
+app.service("addressBookService", function () {
+    var addressBook = [];
+    return {
+        createAddress: function (firstName, lastName, email) {
+            addressBook.push({
+                firstName: firstName,
+                lastName: lastName,
+                email: email
+            });
+        },
+        
+    }
+});
